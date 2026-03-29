f7e4c4ea219700838477bd4f465cddfd27db88c4
Kaushal Raskar <kaushalraskar@gmail.com>
2026-03-29 14:05:27 +0000

    l

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -15,3 +15,17 @@ let someNumber = 69
 let stringNumber = String(someNumber)
 
 console.log(typeof stringNumber) // string
+
+// **********************operations ********
+
+// let value = 3
+// let negValue = -value 
+
+// console.log(negValue)
+
+// let str1 = "kaushal"
+// let str2 = "raskar"
+
+// console.log(str1+str2)
+
+console.log(+true)
