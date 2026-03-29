843fbb15c2f2c471c851a1a296b85903a7352d7c
Kaushal Raskar <kaushalraskar@gmail.com>
2026-03-29 15:18:39 +0000

    s

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+console.log("2">1);
+
+//stack (premitive), Heap (Non premetive)
+
+let myYoutubename = "kaushalcom"
+let anothername = myYoutubename
+console.log(anothername)
+
+let user1 = {
+    email : "sdfghjk.com",
+    upi : "user@sbi"
+}
+
+let user2 = user1
+
+user2.email = "sdfghjk.com"
+
+console.log(user1.email
+)
+console.log(user2.email);
+
---
+++ 01_basics/05_strings.js
@@ -0,0 +1,8 @@
+const name = "kaushal"
+const repoCound = 50
+
+//console.log(name+repoCound+" value");
+
+const gameName = new String ('kaushal')
+console.log(gameName[0]);
+ 
