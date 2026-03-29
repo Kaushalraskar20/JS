5aa94403f51bae786563747637d5267ea9695b30
Kaushal Raskar <kaushalraskar@gmail.com>
2026-03-29 13:11:59 +0000

    k

--- 01_basics/02_datatype.js
+++ 01_basics/02_datatype.js
@@ -20,7 +20,7 @@ console.log(typeof "kaushal")
 
 
 
-
+ 
 
 
 
---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,17 @@
+let score = 33
+
+console.log(typeof score) // number
+
+let isLoggedIn = "" 
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn) // false
+
+// Boolean conversion examples:
+// "" => false 
+// "Kaushal" => true 
+
+let someNumber = 69
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber) // string
