a20432b4c367749e0dc252f50d975cb1587e59ae
Kaushal Raskar <kaushalraskar@gmail.com>
2026-03-29 11:43:42 +0000

    datatype

--- 01_basics/02_datatype.js
+++ 01_basics/02_datatype.js
@@ -1,4 +1,22 @@
+/*
+we need to do focus more on the code reseability 
+*/
+"user strict"
 
+/*
+number => 2 to power 53
+bigint
+string=>""
+boolean=> true/false
+null=>stanalone value 
+undefinedd=> 
+symbol => unique 
+
+*/
+
+//objects 
+
+console.log(typeof "kaushal")
 
 
 
