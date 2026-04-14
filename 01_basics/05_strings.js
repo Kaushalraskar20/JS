cd4c9f3904adf2269fba563d8dbf443c362b023a
Kaushal Raskar <kaushalraskar@gmail.com>
2026-04-14 11:18:21 +0000

    lec11

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -6,3 +6,16 @@ const repoCound = 50
 const gameName = new String ('kaushal')
 console.log(gameName[0]);
  
+console.log(gameName.__proto__)
+console.log(gameName.length);
+
+//this is used for the printing inbetween numbers 
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+//sclice 
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString);
+
+//trim and replace 
+
