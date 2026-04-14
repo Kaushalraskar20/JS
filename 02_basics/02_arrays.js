cca435b5aca3f1cf95ce683217a602df5aa0224a
Kaushal Raskar <kaushalraskar@gmail.com>
2026-04-14 13:09:00 +0000

    operations_array

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,10 @@
+const marvel_heroes = ["thor","Ironman","spiderman"]
+
+const dc_heroes = ["superman","flash","batman "]
+
+marvel_heroes.push(dc_heroes)
+// console.log(marvel_heroes)
+// console.log(marvel_heroes)
+
+ //from methos is used to convert the string elements into the array 
+ console.log(Array.from("Kaushal"))
