993ff95eafbbf04446243320025618cbbcdd7031
Kaushal Raskar <kaushalraskar@gmail.com>
2026-04-19 15:23:30 +0530

    updated

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -1,23 +1,40 @@
-function addTwoNumbers(number1, number2) {
-    let result = number1 + number2;   // define result
-    console.log(result);
-    return result;
-}
+// function addTwoNumbers(number1, number2) {
+//     let result = number1 + number2;   // define result
+//     console.log(result);
+//     return result;
+// }
 
-let output = addTwoNumbers(3, 4);
-console.log("Final Result:", output);
+// let output = addTwoNumbers(3, 4);
+// console.log("Final Result:", output);
 
 
-//**************************** 
+// //**************************** 
 
-function loginUserMessage(username){
-    if(username === undefined){
-            console.log("Enter the use name ");
-            return 
-    }
-    return '${username} just logged in '
-}
-console.log(loginUserMessage());
+// function loginUserMessage(username){
+//     if(username === undefined){
+//             console.log("Enter the use name ");
+//             return 
+//     }
+//     return '${username} just logged in '
+// }
+//console.log(loginUserMessage());
 
 //empty if staemnt willl be consider as the false value 
 
+
+function calculateCartPrice(...number1) { //... this will be called the rest operator and this will store it into the array 
+    return number1;
+}
+
+//console.log(calculateCartPrice(2,3,5,5,6));
+
+const user = {
+    username: "kaushal",
+    price: 199
+}
+
+function handleObject(anyObject) {
+    console.log(`username is ${anyObject.username} and the price is ${anyObject.price}`);
+}
+
+handleObject(user);
