558329cf2882bfef00679d3c7abbfa71d1265140
Kaushal Raskar <kaushalraskar@gmail.com>
2026-04-19 13:54:08 +0530

    k

---
+++ 02_Basics/05_destructure.js
@@ -0,0 +1,19 @@
+//we will do the destructuring represented in the {}  
+
+const course = {
+    coursename : "js in hindi ",
+    price: "999",
+    courseInstructure:"hitesh"
+}
+
+//course.courseInstructure 
+
+const {courseInstructure} = course
+console.log(courseInstructure)
+
+//in json the keys also written in the string 
+// {
+//     "name" : "Kaushal",
+//     "id" : "69"
+// }
+
